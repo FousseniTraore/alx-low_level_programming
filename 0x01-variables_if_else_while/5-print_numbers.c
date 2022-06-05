2c7a24e79e6882180fe03446b6bc45a6be34adfc
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-05 15:17:05 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 10;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", i);
+	}
+		printf("\n");
+	return (0);
+}
+
