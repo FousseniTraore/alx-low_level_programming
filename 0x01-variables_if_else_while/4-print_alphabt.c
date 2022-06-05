67809d9ea3cce133073d83afbd07adeaa1718135
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-05 13:27:39 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char cha;
+
+	for (cha = 'a'; cha <= 'z'; cha++)
+	{
+		if (cha != 'q' && cha != 'e')
+		{
+			putchar(cha);
+		}
+	}
+		putchar('\n');
+	return (0);
+}
