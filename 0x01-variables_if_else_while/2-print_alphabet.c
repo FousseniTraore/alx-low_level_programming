72b5bfbf93fa1fb3befa5c50c5521d2f6ff251c8
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-05 10:27:55 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main -Entry point
  *
@@ -8,10 +8,10 @@ int main(void)
 {
 	char cha;
 
-	for (cha = 'a', cha <= z, cha++)
+	for (cha = 'a'; cha <= 'z'; cha++)
 	{
 		putchar(cha);
-	        putchar('\n');
 	}
+	        putchar('\n');
 	return (0);
 }
