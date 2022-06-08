2347454ff4088bc5b17dceef7d8a8bfdd5c175ea
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-08 14:40:03 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+		_putchar('\n');
+}
