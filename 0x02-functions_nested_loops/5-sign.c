d13aedd3a79eaadd1093cd7115507d1aab9b69ed
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-08 15:48:19 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,24 @@
 #include "main.h"
 /**
- * _isalpha -Return 1 if c is a letter. lowercase or uppercase
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
  *
- * @c: The int to print
+ * @n: the int to print
  * Return: Always 0.
  */
-int _isalpha(int c)
+int print_sign(int n)
 {
-	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		return (0);
-	}
+		_putchar('-');
+		return (-1);
 }
