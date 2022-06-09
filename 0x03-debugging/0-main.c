6de9bdb2335e962ad788f7b8c093ce7bf4dd7c9d
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-09 13:57:02 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
+ * main -Entry point
  * main - tests function that prints if integer is positive or negative
- * Return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
