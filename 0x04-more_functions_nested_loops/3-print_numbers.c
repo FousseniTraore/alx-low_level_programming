68356d1e7a409debb24d8b55a32288a6101d337a
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-10 16:22:08 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints the numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 109; i++)
+	{
+		_putchar("%d", i);
+	}
+	_putchar("\n");
+}
