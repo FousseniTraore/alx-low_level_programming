7d6561f1dcda33d6e4b92b6ea8385f88a269b9fa
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-13 16:44:51 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _putchar - A function that prints a string to the stdout
+ * @c: The variable to print
+ * Return: 1 on success
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
