86f237b9a12d64ca0e31a83b27d0d5e8fa78a8a4
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-16 16:56:35 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - A function that compare two strings
+ * @s1: pointer of string 1
+ * @s2: pointer of string 2
+ * Return: s1[i] - s2[i] which is the dif bet both
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = 0;
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0)
+}
