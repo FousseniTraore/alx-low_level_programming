243baaec30211cb526589111a4573164eb30ae46
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-20 17:16:45 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - a function that locates a substring
+ * @haystack: the searched srring
+ * @needle: the string that we are looking for.
+ * Return: a char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0, b = 0;
+
+	while (haystack[a])
+	{
+		while (needle[b])
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+			b++;
+		}
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+		}
+		a++;
+	}
+	return ('\0');
+}
