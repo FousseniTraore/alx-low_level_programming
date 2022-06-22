f45a87a97c3c91b251b28d225b604df86201da4d
FousseniTraore <foussenitraore0597@gmail.com>
2022-06-21 17:49:52 -0700

    Task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * * set_string - function that sets the value of a pointer to a char
+ * @s: element that will be copied
+ * @to: element to copy
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
