b55ca365b6ce81c49f95304cdd537427a75cd845
FousseniTraore <foussenitraore0597@gmail.com>
2022-07-15 17:20:06 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,32 +8,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	size_t nchar;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	new->str = strdup(str);
-	if (!new->str)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen_recursion(new->str);
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+	new->len = nchar;
 	new->next = *head;
 	*head = new;
-	return (new);
+	return (*head);
 }
-/**
- * _strlen_recursion - returns the length of a string.
- * @s: string.
- * Return: length of @s
- */
-int _strlen_recursion(char *s)
-{
-	if (*s == 0)
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
-}
+
