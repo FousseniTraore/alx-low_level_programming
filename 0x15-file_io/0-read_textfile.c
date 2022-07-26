c825617ff3360fd8b3e292588e4f8e365fc57885
FousseniTraore <foussenitraore0597@gmail.com>
2022-07-26 13:04:45 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,30 +5,41 @@
 /**
  * read_textfile - function that reads a text file and prints
  * @filename: pointer
- * @letters: size of the letters
- * Description: write a function that reads a text file and prints it
- * to the posix standard output.
- * Return: the actual number of letters it could read and print, 0 ptherwise
+ * @letters: number of letters it should read and print.
+ * Return: number of letters it could read and print, or 0 if:
+ * - filename is NULL.
+ * - the file can not be opened or read
+ *   - write fails or does not write the expected amount of bytes.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
+	int file_descriptor = -1;
+	ssize_t output = 0;
+	char *buffer;
 
-	text = malloc(letters);
-	if (text == NULL)
+	if (!filename)
 		return (0);
-	if (filename == NULL)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor < 0)
 		return (0);
-	file = open(filename, O_RDONLY);
-	if (file == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
 	{
-		free(text);
+		close(file_descriptor);
 		return (0);
 	}
-	let = read(file, text, letters);
-	w = write(STDOUT_FILENO, text, let);
-	close(file);
-	return (w);
+	output = read(file_descriptor, buffer, letters);
+	if (output < 0)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+	output = write(STDOUT_FILENO, buffer, output);
+	free(buffer);
+	close(file_descriptor);
+	if (output < 0)
+		return (0);
+	return (output);
 }
