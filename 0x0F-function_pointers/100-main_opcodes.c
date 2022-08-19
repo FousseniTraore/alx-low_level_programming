ba94e4f29d1e67b38ac38a17ee27ecd4c8468b76
FousseniTraore <foussenitraore0597@gmail.com>
2022-08-19 04:49:58 -0700

    Task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,6 +15,12 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
 		exit(2);
 	}
 	for (index = 0; index < bytes; index++)
